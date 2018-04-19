0bf9c2bc8088e2716f24776c52d03046077fff0b
Randium <33968886+Randium@users.noreply.github.com>
2018-04-19 14:13:22 +0200

    Added word count - first step towards money system
    Added a function that keeps track of how much people say.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
+import functions as f
+import random
 
-TOKEN = 'XXXXXXXXXX'
+TOKEN = 'hell_yeah_randium_rocks'
 
 client = discord.Client()
 
@@ -10,8 +12,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    f.add_point(message.author.id,'database.csv')
+
+    if message.content.startswith('@') or message.content.startswith('<'):
+        msg = '{0.author.mention}, it is not very humble to start a message with a mention!'.format(message)
+        await client.send_message(message.channel,msg)
+
+        if random.random() > 0:
+            await client.send_message(message.channel,'...wow. That was ironic.')
+
     if message.content.startswith('!hello'):
         msg = 'Hello there, {0.author.mention}!'.format(message)
+        print('{} said hello to me.'.format(message.author.id))
         await client.send_message(message.channel, msg)
 
 @client.event

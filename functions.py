6d30b849559e3c77a95362ccecfc51f0d3fb1bfa
Randium <33968886+Randium@users.noreply.github.com>
2018-04-19 14:11:49 +0200

    Add word count
    Added a function that counts how many things people say! :D

--- functions.py
+++ functions.py
@@ -1,9 +1,26 @@
-def import_file(file,old_table):
-    bestand = open_file(file,'r')
-    
-    table = old_table
-    
-    for line in bestand:
-        table.append(line.split(','))
-    
-    return table
+import csv
+
+def add_point(player,csv_file):
+
+    # read the file
+    with open(csv_file, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        table = [[e for e in r] for r in reader]
+
+    player_found = False
+    for stored_player in table:
+        if stored_player[0] == player:
+            player_found = True
+            stored_player[1] = int(stored_player[1]) + 1
+            break
+
+    if player_found == False:
+        table.append([player,1])
+        print('New player! {} has just been added to the party!'.format(player))
+
+    print(table)
+
+    # re-write the file
+    with open(str(csv_file), 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        [writer.writerow(r) for r in table]

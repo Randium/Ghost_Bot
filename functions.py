ae97a40914fe6039ab59b419ad20d704b8dc16fb
Randium <33968886+Randium@users.noreply.github.com>
2018-04-22 12:52:11 +0200

    Complete reorganization
    The functions are now created in a more abstract and common manner, allowing functions to be combined and used for multiple purposes rather than each function having one purpose.

--- functions.py
+++ functions.py
@@ -1,45 +1,117 @@
 import csv
 
-def add_point(player,csv_file,amount):
+# Imports a file and presents it as a table.
+# If the table has another table as their only argument, it returns the inner table.
+# The function returns [] is unsuccessful.
+def import_data(csv_file):
 
-    # read the file
-    with open(csv_file, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        table = [[e for e in r] for r in reader]
+    try:
+        with open(csv_file, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            table = [[int(e) for e in r] for r in reader]
+        return table
+    except FileNotFoundError:
+        print("The program called a file that did not exist.")
+        return []
 
-    player_found = False
-    for stored_player in table:
-        if stored_player[0] == player:
-            player_found = True
-            stored_player[1] = int(stored_player[1]) + amount
-            break
+# Writes a table to a given file, effectively overwriting its previous contents.
+# Returns true if successful, false if unsuccessful.
+def save(table,csv_file)
 
-    if player_found == False:
-        table.append([player,amount])
-        print('New player! {} has just been added to the party!'.format(player))
-
-    # re-write the file
-    with open(str(csv_file), 'w') as csvfile:
+    # write it
+    with open(csv_file, 'w') as csvfile:
         writer = csv.writer(csvfile)
         [writer.writerow(r) for r in table]
+    return True
 
-def check_money(player_id,csv_file):
 
-    # read the file
-    with open(csv_file, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        table = [[e for e in r] for r in reader]
+# Changes the score of a certain player on a certain spot. Accepts negative amounts.
+# If the user doesn't have a record yet, then they are added to the database.
+# Returns true if successful, false if unsuccessful.
+def add_score(user_id,spot,amount,fdata):
 
-    for stored_player in table:
-        if stored_player[0] == player_id:
-            return int(stored_player[1])
+    score_table = import_data(fdata)
 
-    # as the command is being counted when checking the balance, one can assume a player will never have 0 messages.
+    for user in score_table:
+        if user[0] == user_id and spot < len(user):
+            user[spot] = int(user[spot]) amount
+            save(score_table,fdata)
+            return True
+
+    print("ERROR: User {} not found.".format(user_id))
+    return False
+
+
+# Takes an emoji as input, and reflects the position of the emoji within the database.
+# Returns 0 if unsuccessful.
+def position(emoji,femoji)
+
+    emoji_table = import_data(femoji)
+
+    for i in range(len(emoji_table)):
+        if emoji_table[i] == emoji:
+            return i + 2
+
     return 0
 
+
+# When given a player and a spot, this function calls how many the user owns of that emoji.
+# Returns 0 if unsuccessful.
+def count(user_id,spot,fdata)
+
+    score_table = import_data(fdata)
+
+    for user in score_table:
+        if user[0] == user_id and spot < len(user):
+            return int(user[spot])
+    return 0
+
+
+# Checks if a file can be converted into an integer.
+# If it cannot, the function returns false.
 def check_for_int(s):
     try:
         int(s)
         return True
     except ValueError:
         return False
+
+
+# Makes the message for the balance command
+# Gives error message if unsuccessful.
+def make_balance(target,fdata):
+
+    # TODO
+    return "ERROR: Could not load the balance of {}.".format(target)
+
+
+# Looks for the best offer for a specified emoji
+# Returns None if none found
+# number_only is a boolean; when True, the function is only to return the price
+def best_offer(fmarket,number_only):
+
+    # TODO
+    return None
+
+
+# Looks for the best request for a specified emoji
+# Returns None if none found
+# number_only is a boolean; when True, the function is only to return the price
+def best_request(fmarket,number_only):
+
+    # TODO
+    return None
+
+
+# Checks if an emoji is a registered emoji in the database.
+# If so, it returns true. If not, it returns false.
+def isvalid(emoji,femoji):
+
+    # TODO
+    return False
+
+
+# This function makes the layout of the specific market page branch.
+def make_market_branch(emoji,fmarket):
+
+    return "ERROR: Could not print out the market page."
